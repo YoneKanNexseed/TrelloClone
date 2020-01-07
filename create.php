5ae7ac78440a58187828e8e4b55725fcc8ecc57a
obscure723 <obscure723@yone-lab.com>
2020-01-07 16:21:02 +0800

    no message

---
+++ create.php
@@ -0,0 +1,27 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+  exit();
+}
+
+if (!array_key_exists('name', $_POST)) {
+  exit();
+}
+
+require_once('./pdo_connect.php');
+require_once('./functions.php');
+require_once('./task_status.php');
+
+$name = h($_POST['name']);
+
+$sql = 'INSERT INTO tasks(name, status, created_at, updated_at) VALUES (:name, :status, now(), now())';
+
+$prepare = $dbh->prepare($sql);
+$prepare->bindValue(':name', $name, PDO::PARAM_STR);
+$prepare->bindValue(':status', TODO, PDO::PARAM_INT);
+
+$result = $prepare->execute();
+
+// response
+header("Access-Control-Allow-Origin: *");
+echo json_encode(['message' => 'success']);
